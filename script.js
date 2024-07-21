4eb74f8edb5ce96ff7bfc2065d568d713e725b6f
Brian rego <brian.rego03@gmail.com>
2024-07-22 03:25:42 +0530

    Load book info into html

--- script.js
+++ script.js
@@ -32,28 +32,37 @@ let latestBook;
 function generateBooks(){
     resetBooks();
     for(let i=0;i<myLibrary.length;i++){
-        
+        let childCounter=0;
         newDiv=document.createElement("div");
         gridSkeleton.appendChild(newDiv).classList.add("books");
         gridSkeleton.children[i].setAttribute('id',`book${i}`);
         latestBook=document.querySelector(`#book${i}`);
         titleDiv=document.createElement("div");
         latestBook.appendChild(titleDiv).classList.add("bookTitle");
+        latestBook.childNodes[childCounter].innerText=myLibrary[i].name;
+        childCounter++;
 
         authorDiv=document.createElement("div");
         latestBook.appendChild(authorDiv).classList.add("author");
+        latestBook.childNodes[childCounter].innerText=myLibrary[i].author;
+        childCounter++;
 
         pageDiv=document.createElement("div");
         latestBook.appendChild(pageDiv).classList.add("totalPages");
+        latestBook.childNodes[childCounter].innerText=myLibrary[i].pages;
+        childCounter++;
 
         modDiv=document.createElement("div");
         latestBook.appendChild(modDiv).classList.add("bookMod");
+        latestBook.childNodes[childCounter].innerText=myLibrary[i].read;
+        childCounter++;
 
         deletionDiv=document.createElement("div");
         latestBook.appendChild(deletionDiv).classList.add("deletion");
 
         readingDiv=document.createElement("div");
         latestBook.appendChild(readingDiv).classList.add("reading");
+        console.dir(latestBook);
 
         
     }
