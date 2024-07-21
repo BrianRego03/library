1dee9262f1afc5d99d6b61850da88931fa7d1bb5
Brian rego <brian.rego03@gmail.com>
2024-07-22 01:42:05 +0530

    Add prototype to add book to library

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function Book(name,author,pages,read){
     this.pages=pages;
     this.read=read;
 }
+
+Book.prototype.addBookToLibrary=function(){
+    myLibrary[myLibrary.length]=this;
+}
+
+const book1=new Book('Autobio','JohnCena',34,true);
+
+const book2=new Book('Autbio','Johena',84,false);
+
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+console.log(myLibrary);
